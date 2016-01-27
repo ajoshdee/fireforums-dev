3645c4cddbd4dfb0a1ee1158fc8b51d095a5b028
ajoshdee <a.dee@klab.com>
2016-01-27 10:36:26 +0800

    Database Setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
