526ba0c374264f944d931342f4ca843599bc4692
ajoshdee <a.dee@klab.com>
2016-01-28 12:00:14 +0800

    View/edit profile

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from flask.ext.login import UserMixin
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -7,4 +8,8 @@ class User(UserMixin, db.Model):
     social_id = db.Column(db.String(64), nullable=False, unique=True)
     nickname = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(64), nullable=True)
+    about_me = db.Column(db.String(140))
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
 
--- app/views.py
+++ app/views.py
@@ -1,22 +1,55 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, flash, g
 from app import app, lm, db
 from flask.ext.login import login_user, logout_user, login_required, current_user
 from .facebook import OAuthSignIn
 from .models import User
+from .forms import EditForm
 
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
-    
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    return render_template('index.html', title='Home')
+    user = g.user
+    return render_template('index.html', title='Home', user=user)
 
 @app.route('/login')
 def login():
     return render_template('login.html', title='Sign In')
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+
+    return render_template('user.html',
+                           user=user)
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
 
 @app.route('/logout')
 def logout():
