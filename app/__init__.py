bea45f85caa4f427244431b660c31b8b002570de
ajoshdee <a.dee@klab.com>
2016-01-29 14:46:20 +0800

    show timestamp in post and comments

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from instance.config import APP_ID, APP_SECRET
+from .momentjs import momentjs
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
@@ -11,6 +12,7 @@ app.config['OAUTH_CREDENTIALS'] = {
         'secret': APP_SECRET
     }
 }
+app.jinja_env.globals['momentjs'] = momentjs
 db = SQLAlchemy(app)
 lm = LoginManager(app)
 lm.login_view = 'login'
